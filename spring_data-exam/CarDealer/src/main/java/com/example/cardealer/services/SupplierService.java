69217dacd67fbf243099c91a419e7644f76c1152
alex <akobachev@gmail.com>
2023-03-09 18:34:48 +0200

    Remove unwanted file

--- spring_data-exam/CarDealer/src/main/java/com/example/cardealer/services/SupplierService.java
+++
@@ -1,7 +0,0 @@
-package com.example.cardealer.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface SupplierService {
-}
--- spring_data-exam/CarDealer/src/test/java/com/example/cardealer/dto/PartDtoTest.java
+++
@@ -1,7 +0,0 @@
-package com.example.cardealer.dto;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class PartDtoTest {
-
-}
