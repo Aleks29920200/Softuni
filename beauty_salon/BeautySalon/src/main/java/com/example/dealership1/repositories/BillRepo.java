d31a6c3e744af0ebdea8f1e6dbfa29f7091175d6
alex <akobachev@gmail.com>
2023-03-09 18:52:46 +0200

    Remove

--- beauty_salon/BeautySalon/src/main/java/com/example/dealership1/repositories/BillRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.dealership1.repositories;
-
-import com.example.dealership1.domain.entity.Bill;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BillRepo extends JpaRepository<Bill, Long> {
-}
--- beauty_salon/BeautySalon/src/main/java/com/example/dealership1/services/RoleService.java
+++
@@ -1,9 +0,0 @@
-package com.example.dealership1.services;
-
-import com.example.dealership1.domain.entity.Role;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface RoleService {
-    void seedAllRoles();
-}
